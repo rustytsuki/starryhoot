e436b5cc0e036e262177dc481a21b32e73417631
RustyTsuki <rustytsuki@gmail.com>
2025-03-28 18:44:10 +0800

    add vscode launch

--- app/src/preload/index.js
+++ app/src/preload/index.js
@@ -5,7 +5,7 @@ function load_roffice() {
     const { platform, arch } = process;
     // console.log(`platform: ${platform}, arch: ${arch}`);
     if (process.env.NODE_ENV === 'development') {
-        return require(path.join(__dirname, `../../../../deploy/roffice/napi-${platform}-${arch}/roffice.node`));
+        return require(path.join(__dirname, `../../../deploy/roffice/x86_64-pc-windows-msvc/lib/roffice.node`));
     } else {
         return require(path.join(__dirname, `../roffice/napi-${platform}-${arch}/roffice.node`));
     }
