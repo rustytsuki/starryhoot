d4f8e670ce06fccd542a3202652e66812b296d43
RustyTsuki <rustytsuki@gmail.com>
2025-03-31 23:40:55 +0800

    multitab

--- app/src/main/tabviews_mgr.js
+++ app/src/main/tabviews_mgr.js
@@ -5,6 +5,7 @@ const TABSBAR_HEIGHT = 46;
 
 let main_window_ = null;
 let tab_views_ = [];
+let path_to_view_indexes_ = {};
 
 export function set_main_window(main_window) {
     main_window_ = main_window;
@@ -36,5 +37,33 @@ function set_home_view(home_view) {
     }
 }
 
+function set_active_index(index) {
+    const view = tab_views_[index];
+    if (view) {
+        main_window_.setBrowserView(view);
+        main_window_.setTopBrowserView(view);
+    }
+}
+
 export function open_file(file_path) {
+    const index = path_to_view_indexes_[file_path];
+    if (index) {
+        set_active_index(index);
+    } else {
+        const editor_view = new BrowserView({
+            webPreferences: {
+                preload: path.join(__dirname, '../preload/editor.js'),
+                sandbox: false,
+            },
+        });
+        const [width, height] = main_window_.getContentSize();
+        editor_view.setBounds({ x: 0, y: TABSBAR_HEIGHT, width: width, height: height - TABSBAR_HEIGHT });
+        editor_view.setAutoResize({ width: true, height: true });
+        editor_view.webContents.loadURL('http://127.0.0.1:65432/about');
+    
+        tab_views_.push(editor_view);
+        const index = tab_views_.length - 1;
+        path_to_view_indexes_[file_path] = index;
+        set_active_index(index);
+    }
 }
