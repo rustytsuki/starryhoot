4ed308d7697d23d53bbdf20e3a08d230d0c8fe07
RustyTsuki <rustytsuki@gmail.com>
2025-04-08 23:35:32 +0800

    fetch kernel

--- deploy/index.js
+++ deploy/index.js
@@ -1,7 +1,9 @@
 import commandLineArgs from 'command-line-args';
 import * as builder from './src/builder.js';
+import { fetch_kernel } from './src/fetch_kernel.js';
 
 const optionDefinitions = [
+    { name: 'fetch', alias: 'f', type: Boolean },
     { name: 'target', alias: 't', type: String },
     { name: 'upload', alias: 'u', type: Boolean },
 ];
@@ -9,6 +11,17 @@ const optionDefinitions = [
 const config = commandLineArgs(optionDefinitions);
 
 async function main() {
+    if (config['fetch']) {
+        let targets = config['target'].split(',');
+        for (let i in targets) {
+            if (await fetch_kernel(targets[i])) {
+                console.error(`fetch kernel: ${targets[i]} failed!`);
+                return 1;
+            }
+        }
+        return 0;
+    }
+
     let targets = config['target'].split(',');
     for (let i in targets) {
         if (await builder.build_electron(targets[i])) {
---
+++ deploy/src/fetch_kernel.js
@@ -0,0 +1,7 @@
+const VER = "0.1213";
+const HASH = "491f2ec";
+
+export async function fetch_kernel(target) {
+
+}
+
