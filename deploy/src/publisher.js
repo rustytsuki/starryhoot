7ec577dc27015d8b8c1eed857652350bea8bfa2b
RustyTsuki <rustytsuki@gmail.com>
2025-04-25 23:51:22 +0800

    deploy

--- deploy/src/publisher.js
+++ deploy/src/publisher.js
@@ -46,9 +46,9 @@ export async function upload_assets(target, pkg) {
         const asset_path = packed_files[i];
         let asset_name = path.basename(asset_path);
         if (target.indexOf('linux') >= 0) {
-            if (target.indexOf('aarch64' >= 0)) {
+            if (target.indexOf('aarch64') >= 0) {
                 asset_name = asset_name.replace(/-linux-arm64(?=\.yml$)/, '');
-            } else if (target.indexOf('x86_64' >= 0)) {
+            } else if (target.indexOf('x86_64') >= 0) {
                 asset_name = asset_name.replace(/-linux(?=\.yml$)/, '');
             }
         } else if (target.indexOf('apple') >= 0) {
