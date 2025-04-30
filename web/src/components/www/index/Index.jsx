2256c550a2c5ee418f4ee360078ca055bbff3c03
RustyTsuki <rustytsuki@gmail.com>
2025-04-30 23:38:35 +0800

    deploy www

--- web/src/components/www/index/Index.jsx
+++ web/src/components/www/index/Index.jsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/Button';
 import { Navigator } from '../Navigator';
 import { auto_download, get_download_button_name } from '../downloader';
 import { ROUTE } from '../ROUTE';
+import { get_full_path } from '../../common/utils/route_util';
 
 export function Index() {
     const [download_bt_name_, set_download_bt_name] = useState('Download');
@@ -27,7 +28,7 @@ export function Index() {
             >
                 {download_bt_name_}
             </Button>
-            <Button variant="link" size="sm" className={styles.other} href={ROUTE.DOWNLOAD}>
+            <Button variant="link" size="sm" className={styles.other} href={get_full_path(ROUTE.DOWNLOAD)}>
                 other platforms
             </Button>
         </>
