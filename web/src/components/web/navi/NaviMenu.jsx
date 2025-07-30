51a0258cc3cab41e028138532b3a4e21016633a6
RustyTsuki <rustytsuki@gmail.com>
2025-07-31 00:50:16 +0800

    navigation bar

--- web/src/components/web/navi/NaviMenu.jsx
+++ web/src/components/web/navi/NaviMenu.jsx
@@ -9,7 +9,7 @@ export const NavMenu = (props) => (
     <NavigationMenu {...props}>
         <NavigationMenuList className="gap-6 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start">
             <NavigationMenuItem>
-                <NavigationMenuLink asChild>
+                <NavigationMenuLink asChild className="hover:bg-transparent focus:bg-transparent active:bg-transparent underline">
                     <Button variant="link">Home</Button>
                 </NavigationMenuLink>
             </NavigationMenuItem>
