c62ad79abc33c68127ad6246d98a98fa5b71737b
RustyTsuki <rustytsuki@gmail.com>
2025-08-05 18:11:21 +0800

    www

--- web/src/components/www/navi/NavigationBar.jsx
+++ web/src/components/www/navi/NavigationBar.jsx
@@ -10,10 +10,12 @@ export const NavigationBar = (props) => {
         <div className="h-screen flex flex-col bg-muted">
             <nav className="h-16 bg-background border-b shrink-0">
                 <div className="h-full flex items-center justify-between max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-                    <Logo />
+                    <div className="flex items-center gap-8">
+                        <Logo />
 
-                    {/* Desktop Menu */}
-                    <NavMenu className="hidden md:block" />
+                        {/* Desktop Menu */}
+                        <NavMenu className="hidden md:block" />
+                    </div>
 
                     <div className="flex items-center gap-3">
                         <Button
