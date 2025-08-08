36748d09dd032b0ea268f522b0ef1f6ecf18777a
RustyTsuki <rustytsuki@gmail.com>
2025-08-08 16:16:14 +0800

    add Faq.pptx

--- web/src/components/www/faq/Faq.jsx
+++ web/src/components/www/faq/Faq.jsx
@@ -1,6 +1,18 @@
+import { useEffect } from 'react';
 import { NavigationBar } from '../navi/NavigationBar';
+import FaqPPTXUrl from './Faq.pptx';
 
 export function Faq() {
+    useEffect(() => {
+        fetch(FaqPPTXUrl)
+            .then((res) => res.arrayBuffer())
+            .then((buffer) => {
+                const bytes = new Uint8Array(buffer);
+                // Do something with bytes
+                console.log('Faq.pptx size is ', bytes.length, 'bytes');
+            });
+    }, []);
+
     return (
         <>
             <NavigationBar>
--- web/vite.config.js
+++ web/vite.config.js
@@ -5,7 +5,7 @@ import vike from 'vike/plugin';
 import { defineConfig } from 'vite';
 import ConditionalCompile from 'vite-plugin-conditional-compiler';
 import { cjsInterop } from 'vite-plugin-cjs-interop';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
     console.log(`load vite config: command: ${command}, mode: ${mode}, ssrBuild: ${isSsrBuild}`);
@@ -58,13 +58,8 @@ export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
         build,
         ssr,
         clearScreen: false,
-        plugins: [
-            react(),
-            ConditionalCompile(),
-            vike(),
-            cjs_interrop,
-            tailwindcss()
-        ],
+        assetsInclude: ['**/*.docx', '**/*.pptx', '**/*.xlsx'],
+        plugins: [react(), ConditionalCompile(), vike(), cjs_interrop, tailwindcss()],
         resolve: {
             alias: {
                 '@': new URL('./', import.meta.url).pathname,
