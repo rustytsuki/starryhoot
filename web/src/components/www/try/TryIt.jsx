cc0c95430a304484115471b4a844bff143cbfa84
RustyTsuki <rustytsuki@gmail.com>
2025-08-12 00:54:09 +0800

    tab try it

--- web/src/components/www/try/TryIt.jsx
+++ web/src/components/www/try/TryIt.jsx
@@ -3,7 +3,9 @@ import { NavigationBar } from '../navi/NavigationBar';
 export function TryIt() {
     return (
         <NavigationBar>
-            Try It
+            <div className="flex w-full h-full items-center justify-center">
+                <div className="w-1/2 h-1/2 bg-gray-200"></div>
+            </div>
         </NavigationBar>
     );
 }
